c78ccffc9c860163089caff49b6626fde9c0c607
Armando Jr <elfiservice@hotmail.com>
2022-12-29 08:38:48 -0300

    more complex animation

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -45,14 +47,18 @@ private fun MyApp(
 @Composable
 fun Greeting(name: String) {
 //    var expanded = remember { mutableStateOf(false) }
-//    val extraPadding by animateDpAsState(
+//    val extraPadding by animateDpAsState(  // https://developer.android.com/jetpack/compose/animation
 //        if (expanded.value) 48.dp else 0.dp
 //    )
 
     // OU
     var expanded by remember { mutableStateOf(false) }
     val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
     )
 
     Surface(
@@ -62,7 +68,7 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
