aef1ce799079877f0eb4a1a90c3633dcafd8ccb7
Armando Jr <elfiservice@hotmail.com>
2022-12-29 08:32:14 -0300

    simple animation

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -43,8 +44,16 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+//    var expanded = remember { mutableStateOf(false) }
+//    val extraPadding by animateDpAsState(
+//        if (expanded.value) 48.dp else 0.dp
+//    )
+
+    // OU
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -59,9 +68,9 @@ fun Greeting(name: String) {
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
