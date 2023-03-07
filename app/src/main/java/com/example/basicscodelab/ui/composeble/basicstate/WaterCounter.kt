84242f804363accc123fd4a9a81398dc3aeefe5a
Armando Jr <elfiservice@hotmail.com>
2023-03-07 08:18:23 -0300

    Memory in a composable function

--- app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WaterCounter.kt
+++ app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WaterCounter.kt
@@ -4,15 +4,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.")
+        // val count: MutableState<Int> = remember { mutableStateOf(0) }
+        // count.value to access
+
+        // using https://kotlinlang.org/docs/delegated-properties.html
+        var count by remember { mutableStateOf(0) }
+
+        Text("You've had ${count} glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
