0da20d046cb9722218fc5d4ef2cc50bb735926c4
Armando Jr <elfiservice@hotmail.com>
2023-03-15 08:10:15 -0300

    Work with lists : provided WellnessTaskList

--- app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WellnessTasksList.kt
@@ -1,3 +1,23 @@
 package com.example.basicscodelab.ui.composeble.basicstate
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.basicscodelab.ui.composeble.basicstate.data.getFakeWellnessTasks
+import com.example.basicscodelab.ui.composeble.basicstate.model.WellnessTask
 
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getFakeWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
--- app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/data/mockData.kt
+++ app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/data/mockData.kt
@@ -2,4 +2,4 @@ package com.example.basicscodelab.ui.composeble.basicstate.data
 
 import com.example.basicscodelab.ui.composeble.basicstate.model.WellnessTask
 
-private fun getFakeWellnessTasks() = List(30) { i -> WellnessTask(id = i, label = "Task # $i") }
+fun getFakeWellnessTasks() = List(30) { i -> WellnessTask(id = i, label = "Task # $i") }
