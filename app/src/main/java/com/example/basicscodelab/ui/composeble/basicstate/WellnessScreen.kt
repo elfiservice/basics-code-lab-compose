62f8a3e9369084df8bd320a25a753cadeab142a9
Armando Jr <elfiservice@hotmail.com>
2023-06-28 08:43:44 -0300

    State in viewModel : Migrate the list and remove method

--- app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WellnessScreen.kt
+++ app/src/main/java/com/example/basicscodelab/ui/composeble/basicstate/WellnessScreen.kt
@@ -2,16 +2,25 @@ package com.example.basicscodelab.ui.composeble.basicstate
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import com.example.basicscodelab.ui.composeble.basicstate.data.getFakeWellnessTasks
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.basicscodelab.ui.composeble.basicstate.viewmodel.WellnessViewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter()
-        val list = remember { getFakeWellnessTasks().toMutableStateList() }
-        WellnessTasksList(modifier, list = list, onCloseTask = { task -> list.remove(task) })
+
+//        val list = remember { getFakeWellnessTasks().toMutableStateList() } ** DO NOT KEEP STATE into COMPOSABLEs
+        // https://developer.android.com/codelabs/jetpack-compose-state?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-state#11
+        // Migrate the list and remove method
+        WellnessTasksList(
+            modifier,
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
     }
 }
